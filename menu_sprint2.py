03f6ed58458e5ceb9de9197dd94adaa5ccac408f
fesilva2109 <felipesilvaa.2109@gmail.com>
2024-05-20 19:01:23 -0300

    Menu Sprint 2

--- menu_sprint2.py
+++ menu_sprint2.py
@@ -1,5 +1,7 @@
 import os 
-os.system("cls")  # Limpa a tela do console (funciona apenas no Windows)
+import re
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 class Carro: 
     def __init__(self, marca, modelo, ano, usuario):
@@ -17,11 +19,6 @@ class Carro:
         #Retorna uma representação em string do objeto Carro.
         return f" Marca: {self.marca} | Modelo: {self.modelo} | Ano: {self.ano}"
 
-    def apresentarCarro():
-        #Apresenta todos os carros criados até o momento.
-        for carro in Carro.carros:
-            print(f"Marca: {carro.marca} | Modelo: {carro.modelo} | Ano: {carro.ano} | Usuário: {carro.usuario.nomeUser}")
-
 
     def listarServicosAgendados(self):
         #Lista todos os serviços agendados pelo usuário.
@@ -38,21 +35,37 @@ class Carro:
     def listarServicosMarcados(self):
         #Lista todos os serviços marcados pelo usuário.
         print("Serviços Marcados:")
-        for servico, _ in self.servicosMarcados:
-            print(servico)
+        for servico, usuario, horario_marcado in self.servicosMarcados:
+            print(f"{servico} ")
 
     def limparServicosMarcados(self):
         #Limpa a lista de serviços marcados pelo usuário.
         self.servicosMarcados.clear
         self.servicosMarcados = {}
 
-    def agendarServico(self,usuario):
-        self.listarServicosMarcados()
-        for servico, _ in self.servicosMarcados:
+    def agendarServico(self):
+        for servico, _, horario_marcado in self.servicosMarcados:
+            self.servicosAgendados.append(servico)
+            self.historicoManutencao.append(servico)
+        self.limparServicosMarcados()
+
+    def adicionarReparoAoHistorico(self):
+        if len(self.historicoManutencao) == 0:
+            print("O registro de manutençôes está vazio")
+            respostaHistorico = input("Gostaria de adicionar uma manutenção ao histórico? (s/n) ")
+        else:
+            print("Histórico de Manutenções:")
+            for servico in self.historicoManutencao:
                 print(servico)
-                self.servicosAgendados.append(servico)
-                
-        self.limparServicosMarcados()       
+            respostaHistorico = input("Gostaria de adicionar uma manutenção ao histórico? (s/n) ")
+        if respostaHistorico.lower() == "s":
+            servico = adicionarServico(self)
+            self.historicoManutencao.append(servico)
+            print("Serviço adicionado")
+        else: 
+            print("Voltando ao menu principal")
+
+
 
 class User:
     def __init__(self, nomeUser, email, telefoneContato):
@@ -79,30 +92,7 @@ class User:
             print(f"{i+1}. {carro}")
         
 
-    def listarServicosAgendados(self):
-        #Lista todos os serviços agendados pelo usuário.
-        print("Serviços Agendados:")
-        for servico in self.servicosAgendados:
-            servico.status = "Agendado"
-            print(servico)
-
-    def limparServicosAgendados(self):
-        #Limpa a lista de serviços agendados do usuário.
-        self.servicosAgendados.clear
-        self.servicosAgendados = {}
-
-    def listarServicosMarcados(self):
-        #Lista todos os serviços marcados pelo usuário.
-        print("Serviços Marcados:")
-        for servico, _  in self.servicosMarcados:
-            print(servico)
-
-    def limparServicosMarcados(self):
-        #Limpa a lista de serviços marcados pelo usuário.
-        self.servicosMarcados.clear
-        self.servicosMarcados = {}
-   
-
+    
 class Servico:
     def __init__(self, nomeServico, preco, idServico):
         #Inicializa um objeto Servico com os atributos especificados.
@@ -110,36 +100,49 @@ class Servico:
         self.preco = preco 
         self.idServico = idServico
         self.status = "Disponível"
+        self.horario_marcado = None
 
     def __str__(self):
         #Retorna uma representação em string do objeto Servico.
-        return f"Serviço: {self.nomeServico} | R${self.preco} |ID: {self.idServico} | Status:{self.status}"
-
+        return f"Serviço: {self.nomeServico} | R${self.preco} |ID: {self.idServico} | Status:{self.status} | Horário: {self.horario_marcado} "
 
 class SistemaDeEncomendas:
     servicosDisponiveis = []  # Lista para armazenar os serviços disponíveis
     servicosAgendados = []  # Lista para armazenar os serviços agendados
-
+    horarios = ["07:00","08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00"]
     def adicionarServicoDisponivel(self, nomeServico, preco, idServico):
         #Adiciona um novo serviço à lista de serviços disponíveis.
         novoServico = Servico(nomeServico, preco, idServico)
         SistemaDeEncomendas.servicosDisponiveis.append(novoServico)
-        
+    
     def pegarIDServico():
         #Retorna o ID do último serviço marcado pelo usuário.
         idServico = usuario.servicosMarcados[-1][0].idServico
         return idServico
-
-    def marcarServico(self, idServico, usuario,carro):
+    def listarHorarios(self):
+        for horario in self.horarios:
+            print(horario)
+    def marcarHorario(self):
+        while True:
+                horario_marcado = input("Digite o horário que deseja marcar (ex: 07:00): ")
+                if horario_marcado in self.horarios:
+                    print(f"Você marcou o horário: {horario_marcado}")
+                    break
+                else:
+                    print("Horário inválido! Por favor, escolha um dos horários disponíveis.")
+        return horario_marcado
+    
+    def marcarServico(self, idServico, usuario,carro, horario_marcado):
         #Marca um serviço para o usuário.
         servico_marcado = None
         for servico in SistemaDeEncomendas.servicosDisponiveis:
             if servico.idServico == idServico:
                 servico_marcado = servico
                 servico.status = "Marcado"
+                servico.horario_marcado = horario_marcado
                 break
-        if servico_marcado:
-            carro.servicosMarcados.append((servico_marcado, usuario))
+        if servico_marcado: 
+            carro.servicosMarcados.append((servico_marcado, usuario, horario_marcado))
             print("Serviço marcado com sucesso")
             return True
         else:
@@ -148,18 +151,104 @@ class SistemaDeEncomendas:
  
 
 ## Funções
+def adicionarServico(carro):
+    servico = input("Qual tipo de serviço foi executado? ")
+    evento = input("Esse evento aconteceu mais de uma vez? (s/n) ")
+
+    while evento.lower() not in ["s", "n"]:
+        print("Opção inválida!")
+        evento = input("Esse evento aconteceu mais de uma vez? (s/n) ")
+
+    if evento.lower() == "s":
+        quantidade_evento = int(input("Quantas vezes? "))
+        print("Adicionando serviço ao histórico de manutenções")
+        return servico, quantidade_evento
+    else:
+        print("Adicionando serviço ao histórico de manutenções")
+        return servico
+
+marcas_validas = ["Toyota","toyota" , "Ford","ford", "Chevrolet","chevrolet", "Volkswagen","volkswagen", "Honda", "honda", "BMW", "bmw"]
+modelos_validos = {
+    "Toyota": ["Corolla", "Camry", "Prius", "corolla", "camry", "prius"],
+    "Ford": ["Fiesta", "Focus", "Mustang","fiesta", "focus", "mustang"],
+    "Chevrolet": ["Cruze", "Malibu", "Impala","cruze", "malibu", "impala"],
+    "Volkswagen": ["Golf", "Jetta", "Passat","golf", "jetta", "passat"],
+    "Honda": ["Civic", "Accord", "Fit","civic", "accord", "fit"],
+    "BMW": ["320i", "X5", "Z4"],
+
+    "toyota": ["Corolla", "Camry", "Prius", "corolla", "camry", "prius"],
+    "ford": ["Fiesta", "Focus", "Mustang","fiesta", "focus", "mustang"],
+    "chevrolet": ["Cruze", "Malibu", "Impala","cruze", "malibu", "impala"],
+    "volkswagen": ["Golf", "Jetta", "Passat","golf", "jetta", "passat"],
+    "honda": ["Civic", "Accord", "Fit","civic", "accord", "fit"],
+    "bmw": ["320i", "X5", "Z4"]
+}
+
+def validar_marca(marca):
+    return marca in marcas_validas
+
+def validar_modelo(marca, modelo):
+    return modelo in modelos_validos.get(marca, [])
+
+def validar_ano(ano):
+    try:
+        ano = int(ano)
+        return 1886 <= ano <= 2024
+    except ValueError:
+        return False    
+
 def cadastrarCarro(usuario):
-    #Permite que o usuário cadastre um novo carro.
-    marca = input("Digite a Marca: ")
-    modelo = input("Digite o Modelo: ")
-    ano = int(input("Digite o Ano: "))
-    usuario.adicionarCarro(marca, modelo, ano)
-    
-    
+    #Permite que o usuário cadastre um novo carro
+    while True:
+        marca = input("Digite a Marca: ")
+        if not validar_marca(marca):
+            print("Marca inválida. Por favor, insira uma marca válida.")
+            continue
+
+        modelo = input("Digite o Modelo: ")
+        if not validar_modelo(marca, modelo):
+            print("Modelo inválido para a marca especificada. Por favor, insira um modelo válido.")
+            continue
+
+        ano = input("Digite o Ano: ")
+        if not validar_ano(ano):
+            print("Ano inválido. Por favor, insira um ano entre 1886 e 2024.")
+            continue
+
+        usuario.adicionarCarro(marca, modelo, int(ano))
+        print("Carro cadastrado!")
+        break
+
+def validar_email(email):
+    padrao_email = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+    return re.match(padrao_email, email) is not None
+
+def validar_telefone(telefone):
+    padrao_telefone = r"^\+?\d{9,15}$"
+    return re.match(padrao_telefone, telefone) is not None
+def validar_nome(nome):
+    padrao_nome = r"^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$"
+    return re.match(padrao_nome, nome) is not None 
 def loginUser():
+ 
     #Cadastra um novo usuário e retorna o objeto User correspondente.
-    while True: 
-        novoUser = User(str(input("Digite seu nome completo: ")), str(input("Digite seu e-mail: ")), str(input("Digite seu telefone para contato: ")))
+   while True:
+        
+        nome = str(input("Digite seu nome completo: "))
+        if not validar_nome(nome):
+            print("Nome inválido. Por favor, insira um nome válido composto apenas por letras e espaços.")
+            continue
+        email = str(input("Digite seu e-mail: "))
+        if not validar_email(email):
+            print("E-mail inválido. Por favor, tente novamente.")
+            continue
+        telefone = str(input("Digite seu telefone para contato: "))
+        if not validar_telefone(telefone):
+            print("Número de telefone inválido. Por favor, tente novamente.")
+            continue
+        
+
+        novoUser = User(nome, email, telefone)
         return novoUser
 
 def manutencaoPreventiva(usuario,carro):
@@ -179,29 +268,27 @@ def manutencaoPreventiva(usuario,carro):
         print("4. Alinhamento e Balanceamento")
         print("5. Troca de Bateria")
         print("6. Sair")
-
         opcaoPreventiva = input("Selecione a opção: ")
-
+        clear_screen()
+        print("Horários Disponíveis: \n")
+        sistema.listarHorarios()
+        horario_marcado = sistema.marcarHorario()
+            
         match opcaoPreventiva:
             case "1":
-                sistema.marcarServico(1, usuario,carro)
-                print("Marcando troca de farol")
+                sistema.marcarServico(1, usuario,carro,horario_marcado)
                 break
             case "2":
-                sistema.marcarServico(2, usuario,carro)
-                print("Marcando troca de óleo")
+                sistema.marcarServico(2, usuario,carro,horario_marcado)
                 break
             case "3":
-                sistema.marcarServico(3, usuario,carro)
-                print("Marcando revisão de freios")
+                sistema.marcarServico(3, usuario,carro,horario_marcado)
                 break
             case "4":
-                sistema.marcarServico(4, usuario,carro)
-                print("Marcando alinhamento e balanceamento")
+                sistema.marcarServico(4, usuario,carro,horario_marcado)
                 break
             case "5":
-                sistema.marcarServico(5, usuario,carro)
-                print("Marcando troca de bateria")
+                sistema.marcarServico(5, usuario,carro,horario_marcado)
                 break
             case "6":
                 print("Saindo")
@@ -209,7 +296,8 @@ def manutencaoPreventiva(usuario,carro):
             case _:
                 print("Opção inválida")
         idServico = int(opcaoPreventiva)
-        sistema.marcarServico(idServico, usuario,carro)
+        
+        sistema.marcarServico(idServico, usuario,carro,horario_marcado)
 
 def pagarServico(carro):
     #Permite que o usuário pague pelos serviços marcados.
@@ -224,7 +312,7 @@ def pagarServico(carro):
             resposta = input("Deseja pagar pelos serviços listados? (s/n): ")
 
         if resposta.lower() == "s":
-            carro.agendarServico(usuario)
+            carro.agendarServico()
             print("Pagamento efetuado com sucesso!")
         else:
             print("Pagamento cancelado.")
@@ -241,22 +329,26 @@ def obterUsuario():
     #Permite que o usuário selecione um usuário da lista.
     listarUsuarios()
     while True:
-        usuario_index = int(input("Selecione o número do usuário: ")) -1
-        if 0 <= usuario_index < len(usuarios):
-            return usuarios[usuario_index]
-        else:
-            print("Por favor, selecione um número de usuário válido.")
-
+        try:
+            usuario_index = int(input("Selecione o número do usuário: ")) -1
+            if 0 <= usuario_index < len(usuarios):
+                return usuarios[usuario_index]
+            else:
+                print("Por favor, selecione um número de usuário válido.")
+        except ValueError:
+                print("Entrada inválida. Por favor, digite um número.")
 def obterCarro():
     #Permite que o usuário selecione um usuário da lista.
     usuario.listarCarros()
     while True:
-        carro_index = int(input("Selecione o número do carro: ")) -1
-        if 0 <= carro_index < len(usuario.carros):
-           
-            return usuario.carros[carro_index]
-        else:
-            print("Por favor, selecione um número de carro válido.")
+        try:
+            carro_index = int(input("Selecione o número do carro: ")) -1
+            if 0 <= carro_index < len(usuario.carros):
+                return usuario.carros[carro_index]
+            else:
+                print("Por favor, selecione um número de carro válido.")
+        except ValueError:
+            print("Entrada inválida. Por favor, digite um número.")
 
 ## Menu Principal
 usuarios = []
@@ -266,15 +358,16 @@ while True:
     print("1. Cadastro de Usuário")
     print("2. Cadastrar Carro")
     print("3. Manutenção Preventiva")
-    print("4. Pagamento de Serviços")
-    print("5. Serviços Marcados")
-    print("6. Serviços Agendados")
-    print("7. Sair")
+    print("4. Histórico de Manutenções")
+    print("5. Pagamento de Serviços")
+    print("6. Serviços Marcados")
+    print("7. Serviços Agendados")
+    print("8. Sair")
 
     opcao = input("Selecione uma opção: ")
 
     if opcao == "1":
-        os.system("cls")
+        clear_screen()
         novoUser = loginUser()
         usuarios.append(novoUser)
         print("Usuário cadastrado com sucesso!")
@@ -282,71 +375,85 @@ while True:
         if len(usuarios) == 0:
             print("Cadastre um usuário primeiro!")
         else:
-            os.system("cls")
+            clear_screen()
             usuario = obterUsuario()
-            os.system("cls")
+            clear_screen()
             novoCarro = cadastrarCarro(usuario)
     elif opcao == "3":
         if len(usuarios) == 0:
             print("Cadastre um usuário primeiro!")
         else:
-            os.system("cls")
+            clear_screen()
             usuario = obterUsuario()
             if len(usuario.carros) == 0:
                 print("Cadastre um carro primeiro!")
             else: 
-                os.system("cls")
-                
+                clear_screen()
                 carro = obterCarro()
-                print(carro)
-                
+                clear_screen()
                 manutencaoPreventiva(usuario,carro)
     elif opcao == "4":
         if len(usuarios) == 0:
             print("Cadastre um usuário primeiro!")
         else:
-            os.system("cls")
+            clear_screen()
             usuario = obterUsuario()
             if len(usuario.carros) == 0:
                 print("Cadastre um carro primeiro!")
             else: 
-                os.system("cls")
+                clear_screen()
                 carro = obterCarro()
-                os.system("cls")
-                pagarServico(carro)
+                clear_screen()
+                carro.adicionarReparoAoHistorico()
+
+
     elif opcao == "5":
         if len(usuarios) == 0:
             print("Cadastre um usuário primeiro!")
         else:
-            os.system("cls")
+            clear_screen()
             usuario = obterUsuario()
             if len(usuario.carros) == 0:
                 print("Cadastre um carro primeiro!")
             else: 
+                clear_screen()
                 carro = obterCarro()
-                if not carro.servicosMarcados :
-                    print ("Nenhum serviço marcado")
-                else:
-                    os.system("cls")
-                    carro.listarServicosMarcados()
-                    print("Seu serviço está marcado, prossiga para o pagamento do serviço para confirma-lo")
+                clear_screen()
+                pagarServico(carro)
     elif opcao == "6":
         if len(usuarios) == 0:
             print("Cadastre um usuário primeiro!")
         else:
-            os.system("cls")
+            clear_screen()
             usuario = obterUsuario()
             if len(usuario.carros) == 0:
                 print("Cadastre um carro primeiro!")
             else: 
-                os.system("cls")
-                obterCarro()
+                clear_screen()
+                carro = obterCarro()
+                if not carro.servicosMarcados :
+                    print ("Nenhum serviço marcado")
+                else:
+                    clear_screen()
+                    carro.listarServicosMarcados()
+                    print("Seu serviço está marcado, prossiga para o pagamento do serviço para confirma-lo")
+    elif opcao == "7":
+        if len(usuarios) == 0:
+            print("Cadastre um usuário primeiro!")
+        else:
+            clear_screen()
+            usuario = obterUsuario()
+            if len(usuario.carros) == 0:
+                print("Cadastre um carro primeiro!")
+            else: 
+                clear_screen()
+                carro = obterCarro()
                 if not carro.servicosAgendados :
                     print ("Nenhum serviço Agendado")
                 else:
-                    os.system("cls")
+                    clear_screen()
                     carro.listarServicosAgendados()
-    elif opcao == "7":
+    elif opcao == "8":
         print("Saindo...")
         break
     else:
